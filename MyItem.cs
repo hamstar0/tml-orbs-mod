998cf1be66205b85b4765d69b70aaef9d812af18
hamstar0 <webutek@gmail.com>
2019-12-23 18:10:47 -0800

    v1.0.0 - Added purification powder conversion of ebonstone/crimstone to into sand * Added purification powder conversion of ebonstone/crimstone to into sand (prehardmode only) * Added mod description, readme, and licenses

--- MyItem.cs
+++ MyItem.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using HamstarHelpers.Services.Timers;
 
 
 namespace Orbs {
@@ -12,6 +14,83 @@ namespace Orbs {
 				var tip = new TooltipLine( this.mod, "OrbsBinoculars", "Reveals patches of orb-resonant terrain (by color)" );
 				tooltips.Add( tip );
 			}
+		}
+
+
+		////////////////
+
+		public override bool Shoot(
+					Item item,
+					Player player,
+					ref Vector2 position,
+					ref float speedX,
+					ref float speedY,
+					ref int type,
+					ref int damage,
+					ref float knockBack ) {
+			if( item.type == ItemID.PurificationPowder && !Main.hardMode ) {
+				var mousePos = new Vector2( Main.mouseX, Main.mouseY );
+				mousePos.X += Main.screenPosition.X;
+				mousePos.Y += Main.screenPosition.Y;
+				var offset = mousePos - player.Center;
+				offset.Normalize();
+				offset *= 3 * 16;
+
+				var tileArea = new Rectangle(
+					((int)(player.Center.X + offset.X) >> 4) - 6,
+					((int)(player.Center.Y + offset.Y) >> 4) - 6,
+					12,
+					12
+				);
+
+				this.PurificationPowderToSand( item.whoAmI + (int)Main.time + player.whoAmI, tileArea );
+			}
+
+			return base.Shoot( item, player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack );
+		}
+
+
+		////
+
+		private void PurificationPowderToSand( int timerCode, Rectangle tileArea ) {
+/*int count = 80;
+Timers.SetTimer( "rect", 10, false, () => {
+	Dust.QuickDust( new Point(tileArea.X, tileArea.Y), Color.Lime );
+	Dust.QuickDust( new Point(tileArea.X, tileArea.Y+tileArea.Height), Color.Lime );
+	Dust.QuickDust( new Point(tileArea.X+tileArea.Width, tileArea.Y), Color.Lime );
+	Dust.QuickDust( new Point(tileArea.X+tileArea.Width, tileArea.Y+tileArea.Height), Color.Lime );
+	return count-- > 0;
+} );*/
+			var evilTiles = new List<(int, int)>();
+
+			int maxX = tileArea.X + tileArea.Width;
+			int maxY = tileArea.Y + tileArea.Height;
+			for( int i = tileArea.X; i < maxX; i++ ) {
+				for( int j = tileArea.Y; j < maxY; j++ ) {
+					Tile tile = Main.tile[i, j];
+					if( tile?.active() != true || (tile.type != TileID.Ebonstone && tile.type != TileID.Crimstone) ) {
+						continue;
+					}
+					evilTiles.Add( (i, j) );
+				}
+			}
+
+			Timers.SetTimer( "OrbsPurificationPowder_"+(timerCode + tileArea.GetHashCode()),
+				60 * 3,
+				false,
+				() => {
+					foreach( (int x, int y) in evilTiles ) {
+						Tile tile = Main.tile[x, y];
+						if( tile?.active() != true || tile.type != TileID.Stone ) {
+							continue;
+						}
+
+						tile.type = TileID.Sand;
+						WorldGen.SquareTileFrame( x, y );
+					}
+					return false;
+				}
+			);
 		}
 	}
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+# Orbs
+
+Removes most tile breaking (except things like wood and ores) and requires using special orb items to clear areas of terrain for passage. These may be acquired in a variety of ways (some requiring other mods), but each type will only work on its corresponding terrain type. Configurable.
+
+https://github.com/hamstar0/tml-orbs-mod
