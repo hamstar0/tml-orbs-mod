7df38491881f5187dab457f0f285bba34a19111d
hamstar0 <webutek@gmail.com>
2020-04-09 11:55:38 -0700

    v1.1.0 - Improved map color chunk display to at least work (WIP)

--- MyMod_Draw.cs
+++ MyMod_Draw.cs
@@ -14,15 +14,30 @@ using Orbs.Items.Base;
 
 namespace Orbs {
 	class UIMapButton : UIPanel {
+		private Color BaseBgColor;
+
+
+
+		////////////////
+
+		public UIMapButton() : base() {
+			this.BaseBgColor = this.BackgroundColor;
+		}
+
+
+		////
+
 		public CalculatedStyle GetMyDimensions() {
 			return new CalculatedStyle(
 				16f,//this.Left.Pixels,
 				16f,//this.Top.Pixels,
-				128f,//this.Width.Pixels,
+				168f,//this.Width.Pixels,
 				32f//this.Height.Pixels
 			);
 		}
 
+
+		////
 
 		public override void Draw( SpriteBatch sb ) {
 			var mydim = this.GetMyDimensions();
@@ -30,8 +45,26 @@ namespace Orbs {
 			ReflectionHelpers.Set( typeof( UIElement ), this, "_dimensions", mydim );
 			this.DrawSelf( sb );
 
-			var textPos = new Vector2( mydim.X, mydim.Y );
+			var textPos = new Vector2( mydim.X + 12, mydim.Y + 4 );
 			sb.DrawString( Main.fontMouseText, "Toggle Orb Colors", textPos, Color.White );
+
+			bool isHovering = false;
+
+			if( Main.mouseX >= mydim.X && Main.mouseX < ( mydim.X + mydim.Width ) ) {
+				if( Main.mouseY >= mydim.Y && Main.mouseY < ( mydim.Y + mydim.Height ) ) {
+					isHovering = true;
+
+					if( Main.mouseLeft && Main.mouseLeftRelease ) {
+						this.Click( null );
+					}
+				}
+			}
+
+			if( isHovering ) {
+				this.BackgroundColor = this.BaseBgColor * 1.2f;
+			} else {
+				this.BackgroundColor = this.BaseBgColor;
+			}
 		}
 	}
 
@@ -62,22 +95,37 @@ namespace Orbs {
 				return;
 			}
 
-			(int x, int y) topLeft = HUDMapHelpers.FindTopLeftTileOfFullscreenMap();
-			topLeft.x = (topLeft.x >> 4) << 4;
-			topLeft.y = (topLeft.y >> 4) << 4;
+			(int x, int y, bool isOnScreen) topLeftTile = HUDMapHelpers.FindTopLeftTileOfFullscreenMap();
+//Main.spriteBatch.DrawString( Main.fontMouseText, "top left: "+topLeft.x+","+topLeft.y, new Vector2(16,400), Color.White );
+			topLeftTile.x = Math.Max( (topLeftTile.x / 16) * 16, 0 );
+			topLeftTile.y = Math.Max( (topLeftTile.y / 16) * 16, 0 );
 			int maxX = Main.maxTilesX;
 			int maxY = Main.maxTilesY;
 
-			for( int y = topLeft.y; y < maxY; y += 16 ) {
-				for( int x = topLeft.x; x < maxX; x += 16 ) {
-					var mapPos = HUDMapHelpers.GetFullMapPositionAsScreenPosition( new Vector2( x << 4, y << 4 ) );
+			bool foundInBoundsY = false;
 
-					if( maxX == Main.maxTilesX && !mapPos.IsOnScreen ) {
-						maxX = (int)mapPos.ScreenPosition.X;
-						break;
+			for( int tileY = topLeftTile.y; tileY < maxY; tileY += 16 ) {
+				bool foundInBoundsX = false;
+
+				for( int tileX = topLeftTile.x; tileX < maxX; tileX += 16 ) {
+					var mapPos = HUDMapHelpers.GetFullMapPositionAsScreenPosition( new Vector2(tileX<<4, tileY<<4) );
+
+					if( foundInBoundsX ) {
+						if( !mapPos.IsOnScreen ) {
+							maxX = tileX;
+							break;
+						}
 					}
 
-					this.DrawMapChunk( x, y, mapPos.ScreenPosition );
+					if( mapPos.IsOnScreen ) {
+						foundInBoundsX = true;
+						foundInBoundsY = true;
+						this.DrawMapChunk( tileX, tileY, mapPos.ScreenPosition );
+					}
+				}
+
+				if( foundInBoundsY && !foundInBoundsX ) {
+					break;
 				}
 			}
 		}
@@ -86,6 +134,27 @@ namespace Orbs {
 		////
 
 		private void DrawMapChunk( int tileX, int tileY, Vector2 screenPos ) {
+			if( tileX < 0 || tileY < 0 || tileX >= Main.maxTilesX || tileY >= Main.maxTilesY ) {
+				return;
+			}
+
+			bool isAir = true;
+			for( int i=tileX; i<tileX+16; i++ ) {
+				for( int j=tileY; j<tileY+16; j++ ) {
+					Tile tile = Main.tile[i, j];
+					if( tile?.active() == true ) {
+						isAir = false;
+						break;
+					}
+				}
+				if( !isAir ) {
+					break;
+				}
+			}
+			if( isAir ) {
+				return;
+			}
+
 			OrbColorCode colorCode = OrbsTile.GetTileColorCode( tileX, tileY );
 			if( colorCode == 0 ) {
 				return;
@@ -96,14 +165,13 @@ namespace Orbs {
 
 			Main.spriteBatch.Draw(
 				texture: Main.magicPixel,
-				position: screenPos,
-				sourceRectangle: null,
-				color: color * 0.5f,
-				rotation: 0f,
-				origin: new Vector2( 16f * scale ),
-				scale: scale,
-				effects: SpriteEffects.None,
-				layerDepth: 1f
+				destinationRectangle: new Rectangle(
+					x: (int)screenPos.X,
+					y: (int)screenPos.Y,
+					width: (int)(16f * scale),
+					height: (int)(16f * scale)
+				),
+				color: color * 0.5f
 			);
 		}
 	}
