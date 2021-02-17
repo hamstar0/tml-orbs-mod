f95d6c62249c7eabf54235503d2b724658011128
hamstar0 <webutek@gmail.com>
2021-02-17 13:19:20 -0800

    v1.3.0 - Restructured config code files more

---
+++ Orbs/MyConfig_Settings_Orbs_Recipes.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+using HamstarHelpers.Classes.Errors;
+
+
+namespace Orbs {
+	public partial class OrbsConfig : ModConfig {
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int BlueOrbRecipeUniqueIngredientCount { get; set; } = 1;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 5 )]
+		public int CyanOrbRecipeUniqueIngredientCount { get; set; } = 5;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 3 )]
+		public int GreenOrbRecipeUniqueIngredientCount { get; set; } = 3;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 5 )]
+		public int PinkOrbRecipeUniqueIngredientCount { get; set; } = 5;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 5 )]
+		public int PurpleOrbRecipeUniqueIngredientCount { get; set; } = 5;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 8 )]
+		public int RedOrbRecipeUniqueIngredientCount { get; set; } = 8;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 5 )]
+		public int BrownOrbRecipeUniqueIngredientCount { get; set; } = 5;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 5 )]
+		public int YellowOrbRecipeUniqueIngredientCount { get; set; } = 5;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int WhiteOrbRecipeUniqueIngredientCount { get; set; } = 1;
+
+		////
+
+		[Range( 0, 99 )]
+		[DefaultValue( 2 )]
+		public int BlueOrbRecipeStack { get; set; } = 2;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int CyanOrbRecipeStack { get; set; } = 1;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int GreenOrbRecipeStack { get; set; } = 1;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int PinkOrbRecipeStack { get; set; } = 1;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int PurpleOrbRecipeStack { get; set; } = 1;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int RedOrbRecipeStack { get; set; } = 1;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int BrownOrbRecipeStack { get; set; } = 1;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int YellowOrbRecipeStack { get; set; } = 1;
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int WhiteOrbRecipeStack { get; set; } = 1;
+	}
+}
---
+++ Orbs/MyConfig_Settings_Orbs_Sources.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+using HamstarHelpers.Classes.Errors;
+
+
+namespace Orbs {
+	public partial class OrbsConfig : ModConfig {
+		[DefaultValue( true )]
+		public bool IsGeoResonantOrbSoldByDryad { get; set; } = true;
+
+		////
+
+		[Range( 0, 99 )]
+		[DefaultValue( 1 )]
+		public int PurpleOrbDropsViaShadowOrb { get; set; } = 1;
+
+
+		////
+
+		[Range( 0f, 1f )]
+		[DefaultValue( 0.5f )]
+		public float AnyOrbPercentChancePerChest { get; set; } = 0.5f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float BlueOrbWeightPerOrbChest { get; set; } = 1f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float CyanOrbWeightPerOrbChest { get; set; } = 1f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float GreenOrbWeightPerOrbChest { get; set; } = 1f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float PinkOrbWeightPerOrbChest { get; set; } = 1f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float PurpleOrbWeightPerOrbChest { get; set; } = 1f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float RedOrbWeightPerOrbChest { get; set; } = 1f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float BrownOrbWeightPerOrbChest { get; set; } = 1f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float WhiteOrbWeightPerOrbChest { get; set; } = 1f;
+
+		[Range( 0f, 100f )]
+		[DefaultValue( 1f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float YellowOrbWeightPerOrbChest { get; set; } = 1f;
+
+
+		[DefaultValue(true)]
+		public bool OnlyGenOrbsInUndergroundChests { get; set; } = true;
+	}
+}
